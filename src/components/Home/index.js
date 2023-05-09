ab841af012f3cf129f95b7495d4afa73d7cad84b
AnastasiaShalukhina <exachka4@gmail.com>
2023-05-10 00:07:02 +0300

    feat: add structure and style for Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,22 @@
-const Home = () => ("Hello");
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import './index.scss';
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className='text-zone'>
+        <h1>Hi, <br /> I'm
+          <img src={LogoTitle} alt='developer' />
+          halukhina Anastasiya
+          <br />
+          frontend developer
+        </h1>
+        <h2>Will build your site with JavaScript/React</h2>
+        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+      </div>
+    </div>
+  )
+};
 
 export default Home;
