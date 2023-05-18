6ad2aaba50b95769656f17fb5b855d2f04300c8a
AnastasiaShalukhina <exachka4@gmail.com>
2023-05-18 13:51:09 +0300

    feat: change timeout for letters and add logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,23 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo/index.js';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
   const surnameArray = ['h', 'a', 'l', 'u', 'k', 'h', 'i', 'n', 'a',];
   const nameArray = ['A', 'n', 'a', 's', 't', 'a', 's', 'i', 'y', 'a'];
   const jobArray = ['f', 'r', 'o', 'n', 't', 'e', 'n', 'd', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+
+  useEffect(() => {
+    const idTimeOut = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 6000)
+
+    return () => clearTimeout(idTimeOut);
+  }, [])
 
   return (
     <div className="container home-page">
@@ -38,6 +47,7 @@ const Home = () => {
         <h2>Will build your site with JavaScript/React</h2>
         <Link to="/contact" className='flat-button'>CONTACT ME</Link>
       </div>
+      <Logo />
     </div>
   )
 };
