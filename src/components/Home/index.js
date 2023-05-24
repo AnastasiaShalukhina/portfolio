fa2b50acd84f45ffa4f5031f9c0829956b537b83
AnastasiaShalukhina <exachka4@gmail.com>
2023-05-24 08:26:52 +0300

    feat: add loaders for pages home and about

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from './Logo/index.js';
+import Loader from 'react-loaders';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -20,35 +21,38 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className='text-zone'>
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt='developer' />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={surnameArray}
-            idx={15} />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={25} />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={35} />
-        </h1>
-        <h2>Will build your site with JavaScript/React</h2>
-        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+    <>
+      <div className="container home-page">
+        <div className='text-zone'>
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt='developer' />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={surnameArray}
+              idx={15} />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={25} />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={35} />
+          </h1>
+          <h2>Will build your site with JavaScript/React</h2>
+          <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type='pacman' />
+    </>
   )
 };
 
